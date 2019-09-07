c9a8741b331cacde62ef300f8a1b6c856c574ab6
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-06 20:57:59 -0700

    feat: initial implementation

---
+++ index.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+const TwitterClient = require('twit')
+const download = require('image-downloader')
+const magick = require('imagemagick')
+const fs = require('fs-extra')
+const homedir = require('os').homedir()
+
+require('dotenv').config()
+
+const t = new TwitterClient({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_KEY_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+const pattern = /http(?:s)?:\/\/(?:www\.)?twitter\.com\/(?<username>[a-zA-Z0-9_]+)\/status\/(?<id>[a-zA-Z0-9_]+)/gi
+
+const args = process.argv.slice(2)
+
+let tweet
+if (!args[0].match(pattern)) {
+  throw new Error(`${args[0]} is not a valid tweet.`)
+} else {
+  tweet = args[0]
+}
+
+const {groups: {username, id}} = pattern.exec(tweet);
+
+t.get('statuses/show/:id', { id }, async (err, data, response) => {
+  const url = data.entities.media[0].media_url_https
+  const dest = process.env.OUT_DEST || `${homedir}/Desktop`
+
+  // Download the image to a specified destination
+  const { filename } = await download.image({ url, dest })
+
+  // Convert the image into a Slack-worthy emoji
+  convertImage(filename, id, dest)
+})
+
+// Scrape the white background away from the image
+const convertImage = (path, id, dest) => {
+  const options = [path, '-fuzz', '10%', '-trim', '-transparent', 'White', `${dest}/${id}.png`]
+
+  magick.convert(options, async err => {
+    if (err) throw err
+    console.log(`Successfully converted ${username}'s tweet with id ${id}.`)
+  
+    // Remove original file
+    await fs.remove(path)
+  })
+}
