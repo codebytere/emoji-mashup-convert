29d45864fef9b68be78cac75263a9e0ccb20d70b
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-13 19:17:00 -0400

    feat: add upload-to-slack

--- index.js
+++ index.js
@@ -2,50 +2,114 @@
 
 const download = require('image-downloader')
 const magick = require('imagemagick')
+const inquirer = require('inquirer')
+const { WebClient } = require('@slack/client')
 const { exec } = require('shelljs')
 const fs = require('fs-extra')
 const homedir = require('os').homedir()
+const fetch = require('node-fetch')
+const FormData = require('form-data');
+
+let workspace
 
 require('colors')
-const pass = '✓'.green
+const pass = '✓'.bold.green
+const fail = '✗'.bold.red
 
-// Let users pass in sketchy tweet urls; we only care about the tweet ID
+// Let users pass in partially-formed tweet urls; we only care about the tweet ID
 const pattern = /twitter\.com\/EmojiMashupBot\/status\/(?<id>[a-zA-Z0-9_]+)/gi
 
-const args = process.argv.slice(2)
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'urlArg',
+    message: 'Enter tweet url:',
+    validate: (input) => {
+      if (!input.match(pattern)) return `${fail} ${input} not a valid tweet.`
+      return true
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'upload',
+    message: 'Upload emoji to Slack workspace?',
+  },
+  {
+    type: 'password',
+    name: 'slackToken',
+    when: (response) => response.upload && !process.env.SLACK_TOKEN,
+    message: 'Enter Slack user token:',
+  },
+]).then(async answers => {
+  // Pull tweet ID from named regex capture group
+  const {groups: {id}} = pattern.exec(answers.urlArg);
 
-let urlArg
-if (!args[0].match(pattern)) {
-  throw new Error(`${args[0]} is not a valid tweet.`)
-} else {
-  urlArg = args[0]
-}
+  // Validate Slack user token
+  const token = process.env.SLACK_TOKEN || answers.slackToken
+  const valid = await validateToken(token)
+  if (!valid) throw new Error(`${fail} Invalid token: ${token}.`)
 
-const {groups: {id}} = pattern.exec(urlArg);
+  // Fetch and parse the image from twitter, ensuring the URL is composed in the format we need
+  const tweetURL = `https://twitter.com/EmojiMashupBot/status/${id}`
+  const execString = `curl -s ${tweetURL} | grep 'property="og:image"' | cut -d'"' -f4`
+  const stdout = exec(execString, { silent: true })
 
-// Fetch and parse the image from twitter, ensuring the URL is composed in the format we need
-const tweetURL = `https://twitter.com/EmojiMashupBot/status/${id}`
-const execString = `curl -s ${tweetURL} | grep 'property="og:image"' | cut -d'"' -f4`
-const stdout = exec(execString, { silent: true })
+  const url = stdout.substring(0, stdout.indexOf(':large'))
+  const dest = process.env.OUT_DEST || `${homedir}/Desktop`
 
-const url = stdout.substring(0, stdout.indexOf(':large'))
-const dest = process.env.OUT_DEST || `${homedir}/Desktop`
+  // Download the image to a specified destination
+  download.image({ url, dest }).then(async ({ filename }) => {
+    // Convert the image into a Slack-worthy emoji
+    await convertImage(filename, id, dest)
 
-// Download the image to a specified destination
-download.image({ url, dest }).then(({ filename }) => {
-  // Convert the image into a Slack-worthy emoji
-  convertImage(filename, id, dest)
+    // Upload emoji to chosen Slack workspace
+    const outPath = `${dest}/${id}.png`
+    if (answers.upload) {
+      const form = new FormData()
+      form.append('token', token)
+      form.append('mode', 'data')
+      form.append('name', 'bell-delete-me')
+      form.append('image', fs.createReadStream(outPath))
+
+      // We need to post to the raw endpoint (not public so not in WebClient)
+      const raw = await fetch(`${workspace.url}api/emoji.add`, {
+        method: 'POST',
+        headers: form.getHeaders(),
+        body: form
+      })
+
+      const response = await raw.json()
+      if (!response.ok) {
+        console.log(`${fail} Upload to Slack workspace ${workspace.team} failed.`)
+      } else {
+        console.log(`${pass} New emoji successfully uploaded to ${workspace.team}.`)
+      }
+    }
+  })
 })
+
+/***** HELPERS *****/
+
+// Ensure token is a va;id user token for a Slack workspace
+const validateToken = async (token) => {
+  const slack = new WebClient()
+  workspace = await slack.auth.test({ token })
+  return workspace.ok
+}
 
 // Scrape the white background away from the image
 const convertImage = (path, id, dest) => {
   const options = [path, '-fuzz', '10%', '-trim', '-transparent', 'White', `${dest}/${id}.png`]
 
-  magick.convert(options, async err => {
-    if (err) throw err
-    console.log(`${pass} Converted image output to: ${dest}/${id}.png`)
-  
-    // Remove original file
-    await fs.remove(path)
+  return new Promise((resolve, reject) => {
+    magick.convert(options, async err => {
+      if (err) reject(err)
+
+      // Remove original file
+      await fs.remove(path)
+
+      console.log(`${pass} See new image at: ${dest}/${id}.png`)
+      resolve()
+    })
   })
 }
